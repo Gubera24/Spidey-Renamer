093fd13262630cd57cddfd6dab8163855e7e0482
Gubera24 <114172003+Gubera24@users.noreply.github.com>
2023-03-24 09:55:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,8 +4,11 @@ API_ID = int(environ.get("API_ID", ""))
 API_HASH = environ.get("API_HASH", "")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 ADMIN = int(environ.get("ADMIN", ""))          
-CAPTION = environ.get("CAPTION", "")
+CAPTION = environ.get("CAPTION", "{file_name}
 
-# for thumbnail ( back end is MrMKN brain 😉)
-DOWNLOAD_LOCATION = "./DOWNLOADS"
+💽 Size = {file_size}
 
+📯Join My Channel : @Tamilan24")
+
+class temp(object):
+    THUMBNAIL = environ.get("THUMBNAIL", "")
